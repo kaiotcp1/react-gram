f839b7e9c63bd40e05041364559df0401b2b9a01
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-02-03 19:19:58 -0300

    Importando Middlewares e iniciando config DB

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,23 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
+// Solve CORS
+app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+
+// Upload directory
+app.use("/uploads". express.static(path.join(__dirname, "/uploads")));
+
+// DB Connection
+require("./config.db.js");
+
+
+// routes
+const router = require('./routes/Router.js');
+
+app.use(router);
+
+
+
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`);
 })
---
+++ backend/routes/Router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express();
+
+// Test route
+
+router.get("/", (req, res) => {
+    res.send("API Working!");
+});
+
+
+
+
+
+
+
+
+module.exports = router;
