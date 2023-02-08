eeaeb3cd78544d938e31c6737833bda922927793
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-02-08 20:07:56 -0300

    Slice do login

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -24,6 +24,21 @@ export const register = createAsyncThunk("auth/register",
     }
 );
 
+// Sing in an user
+export const login = createAsyncThunk("auth/login",
+    async (user, thunkAPI) => {
+        const data = await authService.login(user);
+
+        // check for errors
+        if (data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data;
+    }
+);
+
+
 // Logout an user 
 export const logout = createAsyncThunk("auth/logout", async() => {
     await authService.logout();
@@ -57,6 +72,18 @@ export const authSlice = createSlice({
             state.loading = false;
             state.success = true;
             state.error = null
+            state.user = null;
+        }).addCase(login.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+        }).addCase(login.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null
+            state.user = action.payload;
+        }).addCase(login.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
             state.user = null;
         })
     },
