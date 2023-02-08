9b0d46fdd24f4c93f2967f9a5d9c95c267df9f87
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-02-08 15:32:30 -0300

    Configurando React Router e pastas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 
+// Pages
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+
+// Router
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+
 function App() {
   return (
     <div className="App">
-      <h1>ReactGram</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
