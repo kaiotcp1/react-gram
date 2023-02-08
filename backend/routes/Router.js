aeda78033c4cf439c4540270e04a8921832f3c44
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-02-07 23:54:14 -0300

    [FIX] Corrigindo bugs e adicionando as novas funcionalidades

--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -1,19 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express();
 
-router.use('/api/users', require('./UserRoutes'));
-
-// Test route
-
-router.get("/", (req, res) => {
-    res.send("API Working!");
-});
-
-
-
-
-
-
-
+router.use("/api/users", require("./UserRoutes"));
+router.use("/api/photos", require("./PhotoRoutes"));
 
 module.exports = router;
